07a64b1cd2786098c5fd9e395e6d90d6354218ed
cola <jn123977@163.com>
2021-11-13 10:22:12 +0800

    粘包半包问题

---
+++ src/main/java/com/cola/Netty/Demo/StickyBagAndHalfBag/Server.java
@@ -0,0 +1,47 @@
+package com.cola.Netty.Demo.StickyBagAndHalfBag;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/** 粘包半包问题.
+ * 粘包---TCP滑动窗口
+ */
+@Slf4j
+public class Server {
+    void start() {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            // 调整系统的接受缓冲区（滑动窗口）
+            serverBootstrap.option(ChannelOption.SO_RCVBUF,10);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8999).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server().start();
+    }
+}
