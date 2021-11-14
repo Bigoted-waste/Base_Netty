2c616587f4ddff6d1e9cb07e5d72d6313b5f3e32
cola <jn123977@163.com>
2021-11-14 21:59:46 +0800

    使用properties中参数来判断那种方法的序列化(Java,Json)

---
+++ src/main/java/com/cola/Netty/Demo/ChatRoom/protocol/Serializer.java
@@ -0,0 +1,59 @@
+package com.cola.Netty.Demo.ChatRoom.protocol;
+
+import com.cola.Netty.Demo.ChatRoom.message.Message;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public interface Serializer {
+
+    // 反序列化方法
+    <T> T deserialize(Class<T> clazz, byte[] bytes);
+
+    // 序列化方法
+    <T> byte[] serialize(T object);
+
+    enum Algorithm implements Serializer {
+
+        // java序列化
+        Java {
+            @Override
+            public <T> T deserialize(Class<T> clazz, byte[] bytes) {
+                try {
+                    ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
+                    return (T) ois.readObject();
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException("反序列化失败！", e);
+                }
+            }
+
+            @Override
+            public <T> byte[] serialize(T object) {
+                try {
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    ObjectOutputStream oos = new ObjectOutputStream(bos);
+                    oos.writeObject(object);
+                    return bos.toByteArray();
+                } catch (IOException e) {
+                    throw new RuntimeException("序列化失败", e);
+                }
+            }
+        },
+
+        // json序列化
+        Json {
+            @Override
+            public <T> T deserialize(Class<T> clazz, byte[] bytes) {
+                String json = new String(bytes, StandardCharsets.UTF_8);
+                return new Gson().fromJson(json, clazz);
+            }
+
+            @Override
+            public <T> byte[] serialize(T object) {
+                String json = new Gson().toJson(object);
+                return json.getBytes(StandardCharsets.UTF_8);
+            }
+        }
+    }
+}
